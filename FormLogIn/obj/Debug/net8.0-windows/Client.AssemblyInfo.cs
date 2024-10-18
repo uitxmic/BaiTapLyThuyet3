d772d6b2e6f08a3098807197e31a77f005db7086
Võ Quốc Bảo <51907196+vobao-xD@users.noreply.github.com>
2024-10-18 19:57:25 +0700

    Initial server-side

--- FormLogIn/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
+++ FormLogIn/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("FormLogIn")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42187a576a492375669d19afc17d277d1edce0c5")]
-[assembly: System.Reflection.AssemblyProductAttribute("FormLogIn")]
-[assembly: System.Reflection.AssemblyTitleAttribute("FormLogIn")]
+[assembly: System.Reflection.AssemblyProductAttribute("Client")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
