98447db8e2f26cb9c701c868aaf0e4e7b5e60936
uitxmic <157990036+uitxmic@users.noreply.github.com>
2024-11-16 18:43:31 +0700

    Revert "Push_Initial_Cotroller"
    This reverts commit 78049330d28afcfca7455ba44adc6dab15ee7102.

--- FormLogIn/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
+++ FormLogIn/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c361ccd87a629974f58039fe73dc812a2af877")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4ebaed0092a6acd74308ba2332174b09c50c95")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
