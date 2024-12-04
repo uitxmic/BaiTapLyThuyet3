426df7b422aa6d08edbcf239a7cc7ec6a726a25e
uitxmic <157990036+uitxmic@users.noreply.github.com>
2024-12-04 10:27:28 +0700

    Push

--- FormLogIn/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
+++ FormLogIn/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c176998985a6f00136268420c60df748fdbefda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b5c2c4cc64fc7a656a778ae65b7a4c2ccf160a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
